eaa27bf91ab0d34446618558eb75c8feaf4225ec
elkraken3d <elkraken3d@gmail.com>
2024-03-13 22:24:47 -0300

    agregar el act 13-03

--- 145.js
+++ 145.js
@@ -7,8 +7,6 @@ let stock=0;
 function adivinaAleatorio()
 {
 	var numeroAleatorio = Math.floor((Math.random()*(101-1))+1);
-	/*alert(numeroAleatorio);*/
-	/*document.getElementById('resultado').innerHTML = "Número aleatorio = " + numeroAleatorio;*/
 	var contador = 1;
 	var respuesta = prompt('Introduzca un numero del 1 al 100');
 	while(Number(respuesta)!==numeroAleatorio && contador<30 && Number(respuesta)!=0)  //Number(respuesta)
@@ -27,8 +25,6 @@ function adivinaAleatorio()
 
 		if(respuesta!=null)
 		{
-		/*contador++;*/
-		/*document.getElementById('contador').innerHTML = 'Intentos = ' + contador;*/
 			if(contador==30)
 			{
 			alert('Has superado el número máximo de intentos. Se acabó');
@@ -38,22 +34,13 @@ function adivinaAleatorio()
 			alert('Ganaste');
 			document.getElementById('resultado').innerHTML = "Número aleatorio = " + numeroAleatorio;
 			document.getElementById('contador').innerHTML = "Número aleatorio = " + contador;
-		/*numeroAleatorio=0;
-		contador=0;
-		respuesta=-1;*/
-				/*location.reload();*/
 			}
 		}	
-		/*else
-		{
-		break;
-		}*/
 	
 }
 
 function cargarsilo(){
 	let valor = Number(prompt('Ingresar carga en Tn:'));
-	/*do{*/
 	while(valor!=0){
 		if(valor!=0){
 			silo1(valor);
@@ -70,16 +57,13 @@ function cargarsilo(){
 function silo1(nro){
 	let result01=0;
     let total=stock+nro;
-	alert(total);
-	alert(stock);
-	alert(nro);
     if(total>capacidad){
 		result01=total-capacidad;
         alert("El Silo supera su capacidad maxima por: "+ result01);
     }
     else{
         stock=total;
-        alert("Se ingreso la cantidad de "+nro);
+        alert("Se ingreso la cantidad de "+nro+"\n"+"Se Total en Silo: "+stock);
 
     }
 }
@@ -89,8 +73,8 @@ function silo1(nro){
 function calculoareatriangulo(){
 	let varBase = Number(prompt('Ingresar la base del triangulo:'));
 	let varAltura = Number(prompt('Ingresar la altura de un triangulo: '));
-	let varSuerf=(varAltura+varBase)/2;
-	alert("El area de un triangulo rectangulo es: "+varSuerf);
+	let varSuerf=(varAltura*varBase)/2;
+	alert("La base es: "+varBase+"\n"+"La altura es: "+varAltura+"\n"+"El area de un triangulo rectangulo es: "+varSuerf);
 
 }
 
@@ -102,19 +86,16 @@ function promediodosnros(){
 }
 
 function nromaximo(){
-	let varNro1 = Number(prompt('Ingresar 1º numero:'));
-	let varNro2 = Number(prompt('Ingresar 2º numero'));
-	let varNro3 = Number(prompt('Ingresar 2º numero'));
 	let varMaximo=0;
-	if (varNro1>varNro2){
+	let varNro1 =0;
+	let varContar=0;
+	do{
+	varNro1=Number(prompt('Ingresar un numero:'));
+	if (varNro1>varMaximo){
 		varMaximo=varNro1;
-	}else{
-		varMaximo=varNro2;
-	}
-
-	if(varMaximo<varNro3){
-		varMaximo=varNro2;
-	}
+		}
+	varContar++;
+}while(varContar<3);
 	alert("El valor maximo es: "+varMaximo);
 }
 
